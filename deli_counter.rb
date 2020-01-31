c32245376117e8e6869259f373d5ed5410ed78f5
Samantha Meason <sbmeason@gmail.com>
2020-01-31 00:27:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(deli)
   if deli.length == 0
     puts "The line is currently empty."
   else
-    str = 
+    str = "The line is currently:"
     deli.each.with_index(1) do |person, i|
       puts "#{i}. #{person}"
   end

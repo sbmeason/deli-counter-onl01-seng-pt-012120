d67fea455e40d857be294b12a34b5cfb923837e9
Samantha Meason <sbmeason@gmail.com>
2020-01-31 00:46:34 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(deli)
+  if deli.length == 0
+    puts "The line is currently empty."
+  else
+    str = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      str += " #{i}. #{person}"
+    end
+    puts str
+  end
+end
+
+def take_a_number(deli, person)
+  deli << person
+  puts "Welcome, #{person}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+ if deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{deli[0]}."
+    deli.shift
+  end
+end
